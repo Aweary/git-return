f26053c900b48387173f466879c12d87faae4a7d
Brandon Dail <Aweary@users.noreply.github.com>
2016-02-03 13:05:00 -0600

    Automatically parse git branch

--- git-return.sh
+++ git-return.sh
@@ -2,14 +2,19 @@
 # Checks out the master branch, pulls from a remote origin, then prompts
 # The user to delete the passed branch locally and remotely.
 
+branch_name=`git symbolic-ref --short -q HEAD 2>/dev/null`
+if [ -z "$branch_name" ]; then
+echo "Woops! no git repository found in `pwd`"
+exit
+fi
 read -p "Are you sure you want to delete '"$1"'? (y/n): " answer
 if [ "$answer" = "y" ]; then
   git checkout master;
   git pull;
-  git branch --delete "$1";
+  git branch --delete "$branch_name";
   read -p "Delete '"$1"' from remote origin as well? (y/n): " delete_originy
     if [ "$delete_origin" = "y" ]; then
-      git push origin --delete "$1";
+      git push origin --delete "$branch_name";
     fi
 else
   echo "OK, we wont."
